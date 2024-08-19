9887d9d57c1b14e1dac2fe314f4d5931d4d55025
ice <XunMengWinter@gmail.com>
2024-08-19 20:08:29 +0800

    Initial Commit

---
+++ MatchedGeometryEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchedGeometryEffect
+//
+//  Created by ice on 2024/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchedGeometryEffect/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  MatchedGeometryEffect
+//
+//  Created by ice on 2024/8/19.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
